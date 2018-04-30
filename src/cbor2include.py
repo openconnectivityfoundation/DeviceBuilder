8fa8da8aebf4edd9f89b3259064fdb035c864b09
Wouter van der Beek <wovander@cisco.com>
2018-04-30 14:21:55 +0100

    create a c style include file of the IDD (dat) file

---
+++ src/cbor2include.py
@@ -0,0 +1,91 @@
+#############################
+#
+#    copyright 2018 Open Interconnect Consortium, Inc. All rights reserved.
+#    Redistribution and use in source and binary forms, with or without modification,
+#    are permitted provided that the following conditions are met:
+#    1.  Redistributions of source code must retain the above copyright notice,
+#        this list of conditions and the following disclaimer.
+#    2.  Redistributions in binary form must reproduce the above copyright notice,
+#        this list of conditions and the following disclaimer in the documentation and/or other materials provided
+#        with the distribution.
+#
+#    THIS SOFTWARE IS PROVIDED BY THE OPEN INTERCONNECT CONSORTIUM, INC. "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+#    INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE OR
+#    WARRANTIES OF NON-INFRINGEMENT, ARE DISCLAIMED. IN NO EVENT SHALL THE OPEN INTERCONNECT CONSORTIUM, INC. OR
+#    CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+#    (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+#    OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+#    OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+#    EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+#############################
+
+import argparse
+import os
+from time import gmtime, strftime
+
+import sys
+import traceback
+
+ 
+if sys.version_info < (3, 5):
+    raise Exception("ERROR: Python 3.5 or more is required, you are currently running Python %d.%d!" %
+                    (sys.version_info[0], sys.version_info[1]))
+ 
+print ("*************************")
+print ("***  cbor2include (v1)   ***")
+print ("*************************")
+parser = argparse.ArgumentParser()
+
+parser.add_argument( "-ver", "--verbose", help="Execute in verbose mode", action='store_true')
+parser.add_argument( "-file", "--file", default=None, help="swagger file name",  nargs='?', const="", required=False)
+
+args = parser.parse_args()
+
+print("file        : " + str(args.file))
+
+if (args.file) :
+    cbor_data = open(args.file, 'rb').read()
+
+    if args.verbose:
+        print("cbor data:")
+        print (cbor_data)
+        
+    f = open(args.file+".h", "w")
+
+    size_cbor = len(cbor_data)
+    if args.verbose:
+        print("size:", size_cbor)
+    
+    f.write("// introscpection data\n")
+    f.write("// input file = ")
+    f.write(args.file)
+    f.write("\n")
+    
+    f.write("// date")
+    f.write(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+    f.write("\n\n")
+    
+    f.write("#define CBOR_SIZE ")
+    f.write(str(size_cbor))
+    f.write("  // size of the CBOR\n")
+    
+    f.write("uint8_t introspection_data[] = {\n")
+    counter = 0
+    for item in cbor_data[:-1]:
+        counter += 1
+        int_item = int(item)
+        f.write(" ")
+        f.write(hex(int_item))
+        f.write ("," )
+        if counter == 12:
+             f.write ("\n")
+             counter = 0
+    f.write(hex(int(cbor_data[len(cbor_data)-1])))
+   
+    f.write("};\n\n")
+    f.close()
+    
+
+    
+    
